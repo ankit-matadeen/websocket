713dc7721af51c0702e736bb31bed1788fa00672
Ankit <ankit1999chandgothi@gmail.com>
2026-03-15 10:31:20 +0530

    implement matches post and get route

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
+import { matchRouter } from "./routes/matches";
 import express from express
+
 const app = express()
 const port = 8000;
 app.use(express.json())
+
 app.get("/", (req, res)=>{
     res.send("Hello from express server")
 })
-app.lister(port, ()=>{
+app.listen(port, ()=>{
     console.log(`server is running at https://localhost${port}`)
 })
+app.use('/matches', matchRouter)
